f13ace75664d4a99000a3ad606019692eb354966
Derek Adams <dadams@figure.com>
2022-07-16 10:49:25 -0400

    add apache2 license header. update event fields to new approach

--- model/events.go
+++ model/events.go
@@ -1,7 +1,17 @@
 /**
- * Copyright ©2022 DeviceChain - All Rights Reserved.
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
+ * Copyright © 2022 DeviceChain
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package model
@@ -15,20 +25,20 @@ import (
 
 // Event with token references resolved and info from assignment merged.
 type Event struct {
-	DeviceId        uint
-	EventType       esmodel.EventType
-	OccurredTime    time.Time
-	Source          string
-	AltId           sql.NullString
-	AssignmentId    uint
-	DeviceGroupId   *uint
-	CustomerId      *uint
-	CustomerGroupId *uint
-	AreaId          *uint
-	AreaGroupId     *uint
-	AssetId         *uint
-	AssetGroupId    *uint
-	ProcessedTime   time.Time
+	DeviceId           uint
+	EventType          esmodel.EventType
+	OccurredTime       time.Time
+	Source             string
+	AltId              sql.NullString
+	RelDeviceId        *uint
+	RelDeviceGroupId   *uint
+	RelCustomerId      *uint
+	RelCustomerGroupId *uint
+	RelAreaId          *uint
+	RelAreaGroupId     *uint
+	RelAssetId         *uint
+	RelAssetGroupId    *uint
+	ProcessedTime      time.Time
 }
 
 // Location event fields.
--- processor/EventPersistenceWorker.go
+++ processor/EventPersistenceWorker.go
@@ -1,7 +1,17 @@
 /**
- * Copyright ©2022 DeviceChain - All Rights Reserved.
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
+ * Copyright © 2022 DeviceChain
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package processor
@@ -102,20 +112,20 @@ func (ep *EventPersistenceWorker) PersistLocationEvents(ctx context.Context, eve
 // Persists a resolved event to the datastore.
 func (ep *EventPersistenceWorker) PersistEvent(ctx context.Context, event dmmodel.ResolvedEvent) (*EventPersistenceResults, error) {
 	pevent := model.Event{
-		DeviceId:        event.DeviceId,
-		OccurredTime:    event.OccurredTime,
-		Source:          event.Source,
-		AltId:           rdb.NullStrOf(event.AltId),
-		AssignmentId:    event.AssignmentId,
-		DeviceGroupId:   event.DeviceGroupId,
-		AssetId:         event.AssetId,
-		AssetGroupId:    event.AssetGroupId,
-		CustomerId:      event.CustomerId,
-		CustomerGroupId: event.CustomerGroupId,
-		AreaId:          event.AreaId,
-		AreaGroupId:     event.AreaGroupId,
-		ProcessedTime:   event.ProcessedTime,
-		EventType:       event.EventType,
+		DeviceId:           event.SourceDeviceId,
+		OccurredTime:       event.OccurredTime,
+		Source:             event.Source,
+		AltId:              rdb.NullStrOf(event.AltId),
+		RelDeviceId:        event.TargetDeviceId,
+		RelDeviceGroupId:   event.TargetDeviceGroupId,
+		RelAssetId:         event.TargetAssetId,
+		RelAssetGroupId:    event.TargetAssetGroupId,
+		RelCustomerId:      event.TargetCustomerId,
+		RelCustomerGroupId: event.TargetCustomerGroupId,
+		RelAreaId:          event.TargetAreaId,
+		RelAreaGroupId:     event.TargetAreaGroupId,
+		ProcessedTime:      event.ProcessedTime,
+		EventType:          event.EventType,
 	}
 	switch event.EventType {
 	case esmodel.Location:
